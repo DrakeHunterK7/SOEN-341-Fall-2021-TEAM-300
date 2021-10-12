373abaf991e7efe4d1b51b893a375e87d627b1be
AutoSleep <zl391092@gmail.com>
2021-10-12 15:27:58 -0400

    finish reformatation of code strcture

--- frontend-stack-bubbling/src/App.jsx
+++ frontend-stack-bubbling/src/App.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
-import NavBar from './Components/NavBar/NavBar'
+import { Router, Switch, Route } from "react-router";
+import Navbar from './components/NavBar/NavBar'
+import RegistrationForm from './components/ResigterForm/resigter'
+import LoginForm from './components/LoginForm/login'
+import Home from './components/Home/home'
+import PrivateRoute from './utils/React-Router/privateRoute'
+import AlertComponent from './components/AlertComponent/alertComponent'
+// import privateNavLink from './utils/react-router/privateNavLink'
+
+
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar/>
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container d-flex align-items-center flex-column">
+          <Switch>
+          <Route path="/" exact={true}>
+              <RegistrationForm />
+            </Route>
+            <Route path="/register">
+              <RegistrationForm />
+            </Route>
+            <Route path="/login">
+              <LoginForm />
+            </Route>
+            <PrivateRoute path="/home">
+              <Home/>
+            </PrivateRoute>
+          </Switch>
+          <AlertComponent/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ frontend-stack-bubbling/src/Components/AlertComponent/alertComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class alertComponent extends Component {
+    render() {
+        return (
+            <div>
+                this is an alert
+            </div>
+        )
+    }
+}
+
+export default alertComponent
