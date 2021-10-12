a92e4799602ae71cdf4d1c0932b08150524e79cc
AutoSleep <zl391092@gmail.com>
2021-10-12 15:03:50 -0400

    reformat code strcture

--- frontend-stack-bubbling/src/App.jsx
+++ frontend-stack-bubbling/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import NavBar from './navbar/NavBar'
+import NavBar from './Components/navbar/NavBar'
 
 export class App extends Component {
   render() {
