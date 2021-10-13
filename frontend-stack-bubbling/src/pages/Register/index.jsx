587ec95386e5be7f5015c2955bb04581b0eac10b
AutoSleep <zl391092@gmail.com>
2021-10-13 13:43:10 -0400

    UI desgin for resgister page

--- frontend-stack-bubbling/src/pages/Register/index.jsx
+++ frontend-stack-bubbling/src/pages/Register/index.jsx
@@ -1,10 +1,55 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import './index.css'
 export default class Resigter extends Component {
-	render() {
-		// console.log('About组件收到的props是',this.props);
-		return (
-			<h3>我是Reisgter的内容</h3>
-		)
-	}
+  render() {
+    // console.log('About组件收到的props是',this.props);
+    return (
+      <div className="container margin">
+        <div className="title">Resigter</div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Email Address"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Password"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput2"
+            placeholder="Confirm Password"
+          />
+        </div>
+        <div>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg button-position"
+          >
+            Submit
+          </button>
+          <div className="word">
+            <a href="#">Have a account? Login</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
