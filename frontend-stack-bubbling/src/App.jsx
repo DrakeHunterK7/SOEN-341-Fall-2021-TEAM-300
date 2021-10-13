fa146b597fff0f284bcd32f972e66927a273d51b
AutoSleep <zl391092@gmail.com>
2021-10-13 12:57:02 -0400

    finish initial 4 basic page with 2 button

--- frontend-stack-bubbling/src/App.jsx
+++ frontend-stack-bubbling/src/App.jsx
@@ -1,40 +1,83 @@
 import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router";
-import Navbar from './components/NavBar/NavBar'
-import RegistrationForm from './components/ResigterForm/resigter'
-import LoginForm from './components/LoginForm/login'
-import Home from './components/Home/home'
-import PrivateRoute from './utils/React-Router/privateRoute'
-import AlertComponent from './components/AlertComponent/alertComponent'
-// import privateNavLink from './utils/react-router/privateNavLink'
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./pages/Home"; //Home是路由组件
+import About from "./pages/About"; //About是路由组件
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserProfile from "./pages/UserProfile";
 
 
+import Header from "./components/Header"; //Header是一般组件
+import MyNavLink from "./components/MyNavLink";
 
-export class App extends Component {
+import './app.css'
+
+export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Navbar />
-        <div className="container d-flex align-items-center flex-column">
-          <Switch>
-          <Route path="/" exact={true}>
-              <RegistrationForm />
-            </Route>
-            <Route path="/register">
-              <RegistrationForm />
-            </Route>
-            <Route path="/login">
-              <LoginForm />
-            </Route>
-            <PrivateRoute path="/home">
-              <Home/>
-            </PrivateRoute>
-          </Switch>
-          <AlertComponent/>
+      <div>
+        {/* <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header />
+          </div>
+        </div> */}
+        <div className="row">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <MyNavLink replace to="/about">
+                      About
+                    </MyNavLink>
+                  </li>
+                  <li className="nav-item">
+                    <MyNavLink replace to="/home">
+                      Home
+                    </MyNavLink>
+                  </li>
+                  <li className="nav-item">
+                    <MyNavLink replace to="/login">
+                      Login
+                    </MyNavLink>
+                  </li>
+                  <li className="nav-item">
+                    <MyNavLink replace to="/register">
+                      Register
+                    </MyNavLink>
+                  </li>
+                  <li className="nav-item">
+                    <MyNavLink replace to="/userProfile">
+                      UserProfile
+                    </MyNavLink>
+                  </li>
+                  
+                </ul>
+                <form class="container-fluid justify-content-start">
+                  <button class="btn btn-outline-success me-2" type="button">Login</button>
+                  <button class="btn btn-outline-success me-2" type="button">Logout</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+          <div className="col-xs-6">
+            <div className="panel margin">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Route path="/about" component={About} />
+                  <Route path="/login" component={Login} />
+                  <Route path="/register" component={Register} />
+                  <Route path="/userProfile" component={UserProfile} />
+                  <Redirect to="/home" />
+                </Switch>
+              </div>
+            </div>
+          </div>
         </div>
-      </Router>
+      </div>
     );
   }
 }
-
-export default App;
---
+++ frontend-stack-bubbling/src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+	render() {
+		// console.log('About组件收到的props是',this.props);
+		return (
+			<div className="container">
+                <h1>Login</h1>
+            </div>
+		)
+	}
+}
