f1a8ce7ae938c982519c633c947fed0e5e7ebda3
AutoSleep <zl391092@gmail.com>
2021-10-13 17:41:15 -0400

    finish the function of resgister

--- frontend-stack-bubbling/src/pages/Register/index.jsx
+++ frontend-stack-bubbling/src/pages/Register/index.jsx
@@ -1,54 +1,98 @@
 import React, { Component } from "react";
-import './index.css'
+import "./index.css";
+import axios from "axios";
+
 export default class Resigter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      registaionError: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log(this.state);
+  }
+
+  handleSubmit(e) {
+    const { username, email, password, confirmPassword } = this.state;
+    axios
+      .post("http://localhost:5000/register", {
+        username: username,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      })
+      .then((response) => {
+        console.log("response", response);
+      })
+      .then((err) => {
+        console.log(err);
+      });
+    e.preventDefault();
+  }
+
   render() {
-    // console.log('About组件收到的props是',this.props);
     return (
       <div className="container margin">
         <div className="title">Resigter</div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput"
-            placeholder="Username"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Email Address"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Confirm Password"
-          />
-        </div>
-        <div>
-          <button
-            type="button"
-            className="btn btn-primary btn-lg button-position"
-          >
-            Submit
-          </button>
-          <div className="word">
-            <a href="#">Have a account? Login</a>
+        <form onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange}
+			  className="form-control"
+              required
+            />
           </div>
-        </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={this.state.email}
+              onChange={this.handleChange}
+			  className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+			  className="form-control"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              name="comfirmPassword"
+              placeholder="Comfirm Password"
+              value={this.state.comfirmPassword}
+              onChange={this.handleChange}
+			  className="form-control"
+              required
+            />
+          </div>
+          <button type="submit"
+            className="btn btn-primary btn-lg button-position"> Register</button>
+        </form>
       </div>
     );
   }
