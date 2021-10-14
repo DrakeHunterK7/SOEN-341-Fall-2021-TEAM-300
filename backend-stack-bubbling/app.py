0062459eed5fff94b976cf6f3975acb66891e68d
Yuchen Huang <YuchenHuang97@Gmail.com>
2021-10-14 19:08:50 -0400

    Implemented PostAnswer
    Implemented PostAnswer with token

--- backend-stack-bubbling/app.py
+++ backend-stack-bubbling/app.py
@@ -33,8 +33,13 @@ LoginInfo = reqparse.RequestParser()
 LoginInfo.add_argument('email', help='emailAddress cannot be blank', required=True)
 LoginInfo.add_argument('password', help='Password cannot be blank', required=True)
 
+PostAnswerInfo = reqparse.RequestParser()
+PostAnswerInfo.add_argument('question_id', help='Question_ID cannot be empty', required=True, type=str)
+PostAnswerInfo.add_argument('body', help='Answer body cannot be empty', required=True, type=str)
+
 DB = client["Stack-Bubbling"]
 UserCollection = DB["Users"]
+QuestionCollection = DB["Questions"]
 
 
 class Register(Resource):
@@ -80,10 +85,42 @@ class Login(Resource):
         # write the @jwt_required() before the post and get
 
 
+class PostAnswer(Resource):
+    @jwt_required()
+    def post(self):
+        # Parse the request info
+        info = PostAnswerInfo.parse_args()
+        # Check the identity of User
+        identity = get_jwt_identity()
+        currentUser = None
+        currentUser = UserCollection.find_one({"email" : identity["email"]})
+        if currentUser is None:
+            return make_response(jsonify({"message": "The User identity is invalid"}), 401)
+        # Convert question_id received to uuid
+        # Check if question exist
+        question_id = uuid.UUID(info["question_id"])
+        currentQuestion = None
+        currentQuestion = QuestionCollection.find_one({"_id": question_id})
+        if currentQuestion is None:
+            return make_response(jsonify({"message": "The Question identity is invalid"}), 401)
+        newAnswer = {
+        "_id":uuid.uuid1(),
+        "user_id": currentUser["user_id"],
+        "body": info["body"],
+        "createdAt": datetime.datetime.today(),
+        "is_best_answer": False,
+        "vote_count": 0
+        }
+        # Append the new Answer to answers of question
+        QuestionCollection.update(
+            {"_id": question_id},
+            {"$push": {"answers": newAnswer}})
+        return make_response(jsonify({"message": "The Answer posted successfully"}), 201)
 
 api.add_resource(Register, '/register')
 api.add_resource(Login, '/login')
 
+api.add_resource(PostAnswer, "/postanswer")
 
 if __name__ == "__main__":
     app.debug = True
