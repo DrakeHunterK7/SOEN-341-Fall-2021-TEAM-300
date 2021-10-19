1fe282eabe875cd968374a778273a6cf78615e4f
vrajp434 <90345823+vrajp434@users.noreply.github.com>
2021-10-19 14:12:10 -0400

    Added font styling for post question page

--- frontend-stack-bubbling/src/pages/Home/PostQuestion/index.jsx
+++ frontend-stack-bubbling/src/pages/Home/PostQuestion/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import React2 ,{useState}from 'react'
+import "../../../../src/components/QuestionBox/index.css"
 
-
+var questiontitle = "Please enter your question title";
+var questiontext = "Please enter your question text";
 function App(){
   const [data,setData]=useState(null)
   const [print,setPrint]=useState(false)
@@ -18,6 +20,14 @@ function App(){
       
 
       <div><input type="text" onChange={getData} /></div>
+      
+      <div className="question-title">
+                  <h3>{questiontitle}</h3>
+                </div>
+               
+                <div className="question-text">
+                  <h5>{questiontext}</h5>
+                </div>
 
 
       <button onClick={()=>setPrint(true)} >Post Question</button>
